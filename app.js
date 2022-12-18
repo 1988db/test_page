1d31232dba448c641c49c12124596b099be5a640
1988db <elozrana@gmail.com>
2022-12-18 23:38:47 +0100

    feat: add userAgent recognition

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     let platform = navigator.platform;
     let location = window.location;
-    dupa.innerHTML = 'platform and loaction ' + platform + ' ' + location;
+    let userAgent = navigator.userAgent
+    dupa.innerHTML = 'platform and loaction ' + platform + ' ' + userAgent + ' ' + location;
 })
