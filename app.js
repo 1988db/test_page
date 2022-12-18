da73a11240fa3104bc89d5d4e643ce379e212a6a
1988db <elozrana@gmail.com>
2022-12-18 22:30:03 +0100

    feat: add platform and location recognition

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', ()=> {
+    let platform = navigator.platform;
+    let location = navigator.location;
+    dupa.innerHTML = 'platform and loaction ' + platform + ' ' + location;
+})
