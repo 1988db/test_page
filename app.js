b03447fe0d3661a45a127f53ddd4efbbd768d5cf
1988db <elozrana@gmail.com>
2022-12-18 23:31:47 +0100

    fix: location fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     let platform = navigator.platform;
-    let location = navigator.location;
+    let location = window.location;
     dupa.innerHTML = 'platform and loaction ' + platform + ' ' + location;
 })
